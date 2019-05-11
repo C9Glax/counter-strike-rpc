10364419154e6fabd5b7e8ea99e2c0f24a7dadfa
C9Glax <gfreak70@gmail.com>
2019-05-11 19:51:44 +0200

    Changed Installationpath

--- CSGO Presence/install.bat
+++ CSGO Presence/install.bat
@@ -1,6 +1,7 @@
 echo off
 title "Installation"
 cls
+mkdir "C:\Program Files\CSGORichPresence"
 copy /Y "%~dp0\bin\Release\CSGO Presence.exe" "C:\Program Files\CSGORichPresence\CSGO Presence.exe"
 "%~dp0installutil.exe" "C:\Program Files\CSGORichPresence\CSGO Presence.exe"
 net start CSGORichPresence
